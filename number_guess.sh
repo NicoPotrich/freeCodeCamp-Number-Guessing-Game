ffa8690c58099e7c8c093eaacd9f0bbf4631d251
nicolas potrich <nicolaspotrich3@gmail.com>
2024-08-18 12:57:11 +0000

    feat: welcome the user and add him to the DB

--- number_guess.sh
+++ number_guess.sh
@@ -6,4 +6,16 @@ RANDOM_NUMBER=$((RANDOM % 1000 + 1))
 echo "Enter your username:"
 read USERNAME
 
-USERS=$($PSQL "SELECT name FROM users WHERE username='$USERNAME'")
+USER_INFO=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES ('$USERNAME', 0, 10000)")
+else
+  echo "$USER_INFO" | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+  do 
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
+
