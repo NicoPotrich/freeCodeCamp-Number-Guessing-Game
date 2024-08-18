422a4cf5848168a7033b80a0cef1cfbe9ebb4b1c
nicolas potrich <nicolaspotrich3@gmail.com>
2024-08-18 12:27:41 +0000

    feat: Add random_number in script

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+RANDOM_NUMBER=$((RANDOM % 1000 + 1))
 
 echo "Enter your username:"
 read USERNAME
+
+USERS=$($PSQL "SELECT name FROM users WHERE username='$USERNAME'")
